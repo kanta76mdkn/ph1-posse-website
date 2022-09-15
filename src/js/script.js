4d91b1db26aca95a22ca0f3efc7ae750e4b94068
Kanta Ono <kanta20021476@gmail.com>
2022-09-15 10:28:54 +0900

    feat:クイズページ作成途中

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const trueBox = document.getElementsByClassName('true_box');
+const falseBox = document.getElementsByClassName('false_box');
+
+console.log(trueBox);
+
+trueBox.onclick = trueBox.addEventListener('click',)
+
+if(onclick)
+
+const correctBox = document.getElementsByClassName('');
+const correctBox = document.getElementsByClassName('');
